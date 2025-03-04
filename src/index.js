007b4e9028ec0d78e702d0e37514fe84959b5b63
Divyansh <bharbatdivyansh1@gmail.com>
2025-03-04 09:00:35 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -11,10 +11,11 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ 
-  origin: "https://divyanshapplicationgenerator.vercel.app", 
+
   // origin:"http://localhost:5173",
+  origin:"https://divyanshapplicationgenerator.vercel.app",
   methods: "GET,POST,PUT,DELETE", 
-  credentials: true 
+  credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
